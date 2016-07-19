da456f3f3b153c3fa945955a783a4c44aa15ca52
Dillon Brout <djbrout@midway-login1.rcc.local>
2016-07-19 16:17:26 -0500

    added source directory

---
+++ src/dilltools.py
@@ -0,0 +1,226 @@
+import numpy as np
+#import pyfits as pf
+import os
+import scipy.signal
+import scipy.ndimage as nd
+
+#hello from fermilab2
+# Returns xvals, medians, mads
+def bindata(x, y, bins, returnn=False):
+    medians = np.zeros(len(bins) - 1)
+    mads = np.zeros(len(bins) - 1)
+    nums = np.zeros(len(bins) - 1)
+
+    for i in np.arange(len(bins) - 1):
+        bs = bins[i]
+        bf = bins[i + 1]
+        ww = [(x > bs) & (x < bf)]
+        yhere = y[ww]
+        yhere = yhere[np.isfinite(yhere) & ~np.isnan(yhere)]
+        ss = [abs(yhere) < 3. * np.std(yhere)]
+        try:
+            nums[i] = len(yhere[ss])
+            medians[i] = np.median(yhere[ss])
+            mads[i] = 1.48 * np.median(abs(yhere[ss] - medians[i])) * 1 / np.sqrt(len(yhere[ss]))
+        except IndexError:
+            print('excepted')
+            nums[i] = 0.
+            medians[i] = np.nan
+            mads[i] = np.nan
+    xvals = (bins[1:] + bins[:-1]) / 2.
+    if returnn:
+        return xvals, medians, mads, nums
+    return xvals, medians, mads
+
+# Takes in Filename, reads file columnwise, and returns dictionary such that:
+# import rdcol
+# a = rdcol.read('filename',headline,datastartline)
+# a["Column_Name"] -> returns the list for that column
+#
+# headline and datastartline are always > 0
+#
+# By Dillon Brout
+# dbrout@physics.upenn.edu
+def read(filename, headline, startline, delim=None):
+    linenum = 0
+    go = 0
+    column_list = []
+    return_cols = {}
+    inf = open(filename)
+    for line in inf:
+        line = line.replace('#', '')
+        line = line.strip()
+        cols = line.split(delim)
+        cols[:] = (value for value in cols if value != '')
+        if linenum == headline - 1:
+            for col in cols:
+                return_cols[col.strip()] = []
+                column_list.append(col.strip())
+                go += 1
+        if linenum >= startline - 1:
+            index = 0
+            for col in cols:
+                try:
+                    return_cols[column_list[index]].append(float(col.strip()))
+                except:
+                    return_cols[column_list[index]].append(col.strip())
+                index += 1
+        linenum += 1
+    inf.close()
+    return return_cols
+
+
+def save_fits_image(image,filename):
+    hdu = pf.PrimaryHDU(image)
+    if os.path.exists(filename):
+        os.remove(filename)
+    hdu.writeto(filename)
+
+    return
+
+
+def psfphotometry(im, psf, sky, weight, gal, guess_scale):
+    chisqvec = []
+    fluxvec = []
+
+    galconv = scipy.signal.fftconvolve(gal, psf, mode='same')
+
+    radius = 12
+    substamp = galconv.shape[0]
+    # Make a mask with radius
+    fitrad = np.zeros([substamp, substamp])
+    for x in np.arange(substamp):
+        for y in np.arange(substamp):
+            if np.sqrt((substamp / 2. - x) ** 2 + (substamp / 2. - y) ** 2) < radius:
+                fitrad[int(x), int(y)] = 1.
+
+    if guess_scale is None:
+        for i in np.arange(-10000, 200000, 5):
+            sim = galconv + sky + i * psf
+            chisqvec.append(np.sum((im - sim) ** 2 * weight * fitrad))
+            fluxvec.append(i)
+    else:
+        for i in np.arange(guess_scale - 2000, guess_scale + 2000, 1):
+            sim = galconv + sky + i * psf
+            chisqvec.append(np.sum((im - sim) ** 2 * weight * fitrad))
+            fluxvec.append(i)
+
+    ii = fitrad.ravel()
+    i = ii[ii != 0]
+
+    ndof = len(i) + 1
+
+    fluxvec = np.array(fluxvec)
+    chisqvec = np.array(chisqvec)
+    hh = chisqvec * 0 + min(chisqvec)
+    mchisq = min(chisqvec)
+    idx = np.isclose(chisqvec, hh, atol=1.)
+
+    sim = galconv + sky + fluxvec[chisqvec == min(chisqvec)] * psf
+    sum_data_minus_sim = np.sum(im - sim)
+    return fluxvec[chisqvec == min(chisqvec)], fluxvec[chisqvec == min(chisqvec)] - fluxvec[idx][
+        0], mchisq / ndof, sum_data_minus_sim
+
+
+# Takes in Filename, reads file columnwise, and returns dictionary such that:
+# import rdcol
+# a = rdcol.read('filename',headline,datastartline)
+# a["Column_Name"] -> returns the list for that column
+#
+# headline and datastartline are always > 0
+#
+# By Dillon Brout
+# dbrout@physics.upenn.edu
+
+def readcol(filename,headline=1,startline=2,delim=' '):
+    linenum = 0
+    go = 0
+    column_list = []
+    return_cols = {}
+    inf = open(filename)
+    for line in inf:
+        line = line.replace('#', '')
+        line = line.strip()
+        cols = line.split(delim)
+        cols[:] = (value for value in cols if value != '')
+        if linenum == headline - 1:
+            for col in cols:
+                return_cols[col.strip()] = []
+                column_list.append(col.strip())
+                go += 1
+        if linenum >= startline - 1:
+            index = 0
+            for col in cols:
+                try:
+                    return_cols[column_list[index]].append(float(col.strip()))
+                except:
+                    return_cols[column_list[index]].append(col.strip())
+                index += 1
+        linenum += 1
+    inf.close()
+    for k in return_cols.keys():
+        return_cols[k] = np.array(return_cols[k])
+    return return_cols
+
+
+def pixelate(matrix, pixelation_factor):
+    zmatrix = nd.interpolation.zoom(matrix, 1. / float(pixelation_factor))
+    return zmatrix
+
+class tmpwriter():
+    # tempdir = location to write files
+    # tmp_index = index for parallel computation to avoid over-writing files
+    def __init__(self, tempdir='./tmp/',tmp_subscript=0,usedccp=False):
+        self.tmpdir = tempdir
+        self.tmp_index = str(tmp_subscript)
+        self.usedccp = usedccp
+    def writefile(self,text,filename):
+        tempfile = os.path.join(self.tmpdir, 'tmp_' + self.tmp_index + '.txt')
+        if os.path.isfile(tempfile):
+            os.remove(tempfile)
+        if os.path.isfile(filename):
+            os.remove(filename)
+
+        a = open(tempfile,'w')
+        a.write(text)
+        a.close()
+        if self.usedccp:
+            os.system('dccp ' + tempfile + ' ' + filename)
+        else:
+            os.system('mv ' + tempfile + ' ' + filename)
+
+        print('saved', filename)
+
+    def appendfile(self,text,filename):
+        tempfile  = os.path.join(self.tmpdir, 'tmp_' + self.tmp_index + '.txt')
+        if os.path.isfile(tempfile):
+            os.remove(tempfile)
+        if self.usedccp:
+            os.system('dccp ' + filename + ' ' + tempfile)
+        else:
+            os.system('mv ' + filename + ' ' + tempfile)
+
+        a = open(tempfile,'a')
+        a.write(text)
+        a.close()
+        if os.path.isfile(filename):
+            os.remove(filename)
+        if self.usedccp:
+            os.system('dccp ' + tempfile + ' ' + filename)
+        else:
+            os.system('mv ' + tempfile + ' ' + filename)
+
+
+    def savez(self,filename,**kwargs):
+        tempfile  = os.path.join(self.tmpdir, 'tmp_' + self.tmp_index + '.npz')
+        if os.path.isfile(tempfile):
+            os.remove(tempfile)
+        if os.path.isfile(filename):
+            os.remove(filename)
+        np.savez(tempfile,**kwargs)
+        if self.usedccp:
+            os.system('dccp ' + tempfile + ' ' + filename)
+        else:
+            os.system('mv ' + tempfile + ' ' + filename)
+        print('saved',filename)
+
---
+++ src/gensnccdata.py
@@ -0,0 +1,361 @@
+import os
+import matplotlib as m
+import multiprocessing as mp
+
+
+m.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+
+np.set_printoptions(threshold=np.nan)
+import sys
+import sncosmo
+import bazin
+
+
+#SALT AND BAZIN MUST BE THE FIRST TWO SOURCES AND ONLY IF THEY WORK WILL THE REST OF THE SOURCES BE FIT!
+sources = [    'salt2', 's11-2004hx', 's11-2005lc', 's11-2005hl', 's11-2005hm',
+               's11-2005gi', 's11-2006fo', 's11-2006jl', 'hsiao', 'hsiao-subsampled', 'snf-2011fe', 'snf-2011fe',
+               'snana-2004gq', 'snana-sdss004012', 'snana-2006fo', 'snana-sdss014475', 'snana-2006lc', 'snana-2007ms',
+               'snana-04d1la', 'snana-04d4jv', 'snana-2004gv', 'snana-2006ep', 'snana-2007y', 'snana-2004ib',
+               'snana-2005hm', 'snana-2006jo', 'snana-2007nc', 'snana-2004hx', 'snana-2005gi', 'snana-2006gq',
+               'snana-2006kn', 'snana-2006jl', 'snana-2006iw', 'snana-2006kv', 'snana-2006ns', 'snana-2007iz',
+               'snana-2007nr', 'snana-2007kw', 'snana-2007ky', 'snana-2007lj', 'snana-2007lb', 'snana-2007ll',
+               'snana-2007nw', 'snana-2007ld', 'snana-2007md', 'snana-2007lz', 'snana-2007lx', 'snana-2007og',
+               'snana-2007ny', 'snana-2007nv', 'snana-2007pg', 'snana-2006ez', 'snana-2006ix', 'whalen-z15b',
+               'whalen-z15g', 'whalen-z25b', 'whalen-z25d', 'whalen-z25g', 'whalen-z40b', 'whalen-z40g',
+               'nugent-hyper','nugent-sn2p','nugent-sn2l','nugent-sn2n','mlcs2k2'
+               ]
+
+
+def grabdata(loc,list):
+    f = open(list,'r').readlines()
+    files = []
+    for file in f:
+        files.append(os.path.join(loc,file.strip()))
+
+    lightcurves = []
+    hostzs = []
+    hostz_stds = []
+    snids = []
+    sntypes = []
+    params = []
+
+    max = 20000
+    cntr = 0
+
+    for file in files:
+        cntr += 1
+        if cntr > max:
+            continue
+        sn = open(file,'r').readlines()
+        keepgoing = True
+        i = 0
+        sntype = -999
+        snid = -999
+        hostz = -999
+        hostz_std = -999
+        while keepgoing:
+            if 'SNID:' in sn[i]:
+                snid = sn[i].split()[1]
+            if 'SNTYPE:' in sn[i]:
+                sntype = sn[i].split()[1]
+            if 'HOST_GALAXY_PHOTO-Z:' in sn[i]:
+                hostz = float(sn[i].split()[1])
+                hostz_std = float(sn[i].split()[3])
+                keepgoing = False
+            i += 1
+
+        hostzs.append(hostz)
+        hostz_stds.append(hostz_std)
+        snids.append(snid)
+        sntypes.append(sntype)
+
+        sndata = dt.read(file,23,24)
+        flux = sndata['FLUXCAL']
+        fluxerr = sndata['FLUXCALERR']
+        filt = sndata['FLT']
+        mjd = sndata['MJD']
+
+        badrow = -9
+        for i,f,fe,flt,m in zip(range(len(flux)),flux,fluxerr,filt,mjd):
+            if 'g+r+i+z' in str(f):
+                badrow = i
+            elif 'g+r+i+z' in str(fe):
+                badrow = i
+            elif 'g+r+i+z' in str(flt):
+                badrow = i
+            elif 'g+r+i+z' in str(m):
+                badrow = i
+        if badrow != -9:
+            flux = np.delete(flux,badrow)
+            fluxerr = np.delete(fluxerr,badrow)
+            filt = np.delete(filt, badrow)
+            mjd = np.delete(mjd, badrow)
+
+        cosmofile = file.split('.')[0]+'.sncosmo'
+        out = open(cosmofile,'w')
+        out.write('time band flux fluxerr zp zpsys\n')
+        for i,f,fe,flt,m in zip(range(len(flux)),flux,fluxerr,filt,mjd):
+            out.write(str(m)+' des'+str(flt)+' '+str(f)+' '+str(fe)+' 27.5 ab\n')
+        out.close()
+        lc = sncosmo.read_lc(cosmofile)
+        lightcurves.append(lc)
+
+        params.append({'snid':snid,'type':sntype,'hostz':hostz,'hostz_std':hostz_std})
+        print(snid,sntype,hostz,hostz_std)
+
+    hostzs = np.array(hostzs)
+    hostz_stds = np.array(hostz_std)
+    snids = np.array(snids)
+    sntypes = np.array(sntypes,dtype='float')
+
+    print('unique types',np.unique(sntypes))
+
+    dsntypes = np.zeros(len(sntypes))
+    dsntypes[sntypes > 1] = np.ones(len(sntypes[sntypes > 1]))
+    dsntypes[sntypes == 1] = np.zeros(len(sntypes[sntypes == 1]))
+    dsntypes[sntypes == -9] = np.zeros(len(sntypes[sntypes == -9]))-9
+
+    print('nonia',len(dsntypes[dsntypes == 1]))
+    print('ia',len(dsntypes[dsntypes == 0]))
+    print('none',len(dsntypes[dsntypes == -9]))
+
+    'FEED ONLY LCs WITH NON -9 TYPES THROUGH ALL THE LIGHCURVE FITS AND SPLIT THAT INTO TEST AND TRAIN'
+    knownlightcurves = []
+    knownparams = []
+    knowntypes = []
+    blindlightcurves = []
+    blindparams = []
+    for lc,t,p in zip(lightcurves,dsntypes,params):
+        if t == -9:
+            blindlightcurves.append(lc)
+            blindparams.append(p)
+        else:
+            knownlightcurves.append(lc)
+            knownparams.append(p)
+            knowntypes.append(t)
+
+    ndim = 1 + 1 + 1 + len(sources) + 2
+    fitparams = np.zeros((len(knownlightcurves), ndim)) #THIS NEEDS TO CUT OUT -9S
+
+    fitparams, truetypes = lcfit(knownlightcurves, knownparams, fitparams, knowntypes, sources)
+    print('fitparams.shape', fitparams.shape)
+    print('truetypes.shape', truetypes.shape)
+    np.savez('sncc_known2.npz', big_data_array=fitparams, truetypes=truetypes, params=knownparams)
+    print('saved sncc_known2.npz')
+    return
+
+
+
+def lcfit(lightcurves,params,fitparams,truearray,sources):
+    models = []
+    for source in sources:
+        try:
+            models.append(sncosmo.Model(source=source))
+        except:
+            models.append('custom_bazin_model')
+    iatot = 0.
+    noniatot = 0.
+    iafailed = 0.
+    noniafailed= 0.
+    badrows = []
+    for i,lc in enumerate(lightcurves):
+        if truearray[i] == 0:
+            iatot += 1.
+        else:
+            noniatot += 1.
+
+        z = params[i]['hostz']
+        ze = params[i]['hostz_std']
+        fitparams[i,-2] = z
+        fitparams[i,-1] = ze
+        t0 = lc['time'][np.argmax(lc['flux'])]
+        print('Fitting LC '+str(i)+'/'+str(len(lightcurves)),'t0',t0,'z',z)
+        input_param_index = 3
+
+        saltworked = True
+        bazinworked = True
+        for j, model, source in zip(range(len(sources)), models, sources):
+
+            if source == 'salt2':
+                try:
+                    #model.set(z=z)
+                    res, fitted_model = sncosmo.fit_lc(lc, model,['z','t0', 'x0', 'x1', 'c'],
+                                               bounds={'z':(z-3.*ze,z+3.*ze ),'t0':(t0-50.,t0+50.)})
+                    #print(res.keys())
+                    print('Type',truearray[i],'Source', source,'Fit Chisq', res.chisq / res.ndof,
+                          'Fit c',res.parameters[4],'Fit x0',res.parameters[2],'Fit x1',res.parameters[3])
+                    #print('chisq/ndof',res.chisq/res.ndof)
+                    #print('param names',res.param_names[:])
+                    #print('params',res.parameters[:])
+                    #print('-'*100)
+
+                    fitparams[i,:4] = np.array([res.parameters[2], res.parameters[3], res.parameters[4], res.chisq/res.ndof])
+                    t0 = res.parameters[1]
+                    #fitparams[i, :] = np.array([res.parameters[4], res.chisq / res.ndof])
+                    #sys.exit()
+                except sncosmo.fitting.DataQualityError:
+                    #fitparams[i, :] = np.array([99, 99, 99, 99])
+                    # if truearray[i] == 0:
+                    #     print('could not fit Type Ia')
+                    #     sys.exit()
+                    print('fitting failed: SN Type ',truearray[i])
+                    fitparams[i, :4] = np.array([0., 0., 0., 500.])
+                    print('-'*100)
+                    if truearray[i] == 0:
+                        iafailed += 1.
+                    else:
+                        noniafailed += 1.
+                    #badrows.append(i)
+                    #saltworked = False
+
+                except RuntimeError:
+                    # if truearray[i] == 0:
+                    #     print('could not fit Type Ia')
+                    #     sys.exit()
+                    #fitparams[i, :] = np.array([99, 99, 99, 99])
+                    fitparams[i, :4] = np.array([0., 0., 0., 500.])
+                    print('fitting failed: SN Type ',truearray[i])
+                    print('-'*100)
+                    #badrows.append(i)
+                    #saltworked = False
+                    if truearray[i] == 0:
+                        iafailed += 1.
+                    else:
+                        noniafailed += 1.
+            elif saltworked:
+                if source == 'bazin':
+                    if True:
+                        flux = lc['flux']
+                        fluxerr = lc['fluxerr']
+                        bands = lc['band']
+                        mjds = lc['time']
+                        input_param_index = 3
+                        for band in np.sort(np.unique(bands)):
+                            bflux = flux[bands==band]
+                            bfluxerr = fluxerr[bands==band]
+                            bmjds = mjds[bands==band]
+                            try:
+                                chisq,popt = bazin.lmfit(bmjds,bflux,bfluxerr)
+                                if chisq > 500.:
+                                    chisq = 500.
+                                print('Source Bazin',band,'Fit Chisq',chisq,'t0',popt[0],'tau_fall',popt[1],
+                                      'tau_rise',popt[2],'A',popt[3])
+                            except:
+                                print('BASIN FAILED ' * 10)
+                                chisq, popt = 500.,[0,0,0,0,0]
+                            fitparams[i,j+input_param_index] = chisq
+                            input_param_index += 1
+                            fitparams[i, j+input_param_index] = popt[1]
+                            input_param_index += 1
+                            fitparams[i, j+input_param_index] = popt[2]
+                            input_param_index += 1
+                            fitparams[i, j+input_param_index] = popt[3]
+                            input_param_index += 1
+                            fitparams[i, j+input_param_index] = popt[4]
+                            input_param_index += 1
+                        input_param_index -= 1
+
+                else:
+                    if bazinworked:
+                        if not source == 'mlcs2k2':
+                            #print(model.param_names)
+                            try:
+                                res, fitted_model = sncosmo.fit_lc(lc, model, ['z', 't0', 'amplitude'],
+                                                                   bounds={'z':(z-3.*ze,z+3.*ze ),
+                                                                           't0': (t0 - 20., t0 + 20.)})
+                                                                   #bounds={'z': (z - .05 * z,
+                                                                   #              z + .05 * z)})  # assume we know hostgal z to within 5%
+                                print('Source',source,'Fit Chisq',res.chisq/res.ndof)
+                                fitparams[i, j+input_param_index] = res.chisq/res.ndof
+                            except:
+                                print('Source', source, 'Could not Fit... Chisq', 300.)
+                                fitparams[i, j+input_param_index] = 300.
+                        else:
+                            try:
+                                res, fitted_model = sncosmo.fit_lc(lc, model, ['z', 't0', 'amplitude', 'delta',],
+                                                                   bounds={'z':(z-3.*ze,z+3.*ze ),
+                                                                           't0': (t0 - 20., t0 + 20.)})
+                                                                   #bounds={'z': (z - .05 * z,
+                                                                   #              z + .05 * z)})  # assume we know hostgal z to within 5%
+                                print('Source', source, 'Fit Chisq', res.chisq / res.ndof)
+                                fitparams[i, j + input_param_index] = res.chisq / res.ndof
+                            except:
+                                print('Source', source, 'Could not Fit... Chisq', 300.)
+                                fitparams[i, j + input_param_index] = 300.
+
+        print(fitparams[i,:])
+        print('-' * 100)
+
+    print('Percentage of Ia Failed',iafailed/iatot)
+    print('Percentage of NONIa Failed',noniafailed/noniatot)
+    fitparams = np.delete(fitparams,np.array(badrows),axis=0)
+    truetypes = np.delete(truearray,np.array(badrows))
+
+
+    return fitparams, truetypes
+
+
+
+# START MULTIPROCESSING TECHNOLOGY
+def mapVoxelization(gal_zed,voxel_limits_z,unique_pix,pix,numprocessors=2,verbose=False):
+    if verbose: print 'Mapping voxelization. Number of processors available is :',numprocessors
+    indices = range(unique_pix.size)
+    voxelcounts = []
+    voxelmeans = []
+
+    voxelated_gals = dict()
+    voxelated_gals["pixels"] = unique_pix
+
+    index = 0
+    keeplooping = True
+
+    while keeplooping:
+        output = mp.Queue()
+        ileft = np.min([unique_pix.size-index,numprocessors])
+        processes = [mp.Process(target=maploop, args=(x, gal_zed,voxel_limits_z,unique_pix,pix, output)) for x in range(index,index+ileft)]
+        for p in processes:
+            p.start()
+        for p in processes:
+            p.join(1)
+
+        for x in range(index,index+ileft):
+            i,vc,vm = output.get()
+            upix = unique_pix[i]
+            voxelated_gals[upix] = vc
+            voxelated_gals[upix,"mean_z"] = vm
+
+        index += ileft
+        if index > unique_pix.size-2:
+            keeplooping = False
+
+    return voxelated_gals
+
+def maploop(i,gal_zed, voxel_limits_z, unique_pix, pix, output,verbose=False) :
+    if verbose: print 'running loop ',i
+
+    upix = unique_pix[i]
+    ix = upix == pix
+    voxels = np.zeros(voxel_limits_z.shape[0])
+    voxelmeans = np.zeros(voxel_limits_z.shape[0])
+    for j in range(0,voxel_limits_z.shape[0]):
+        zed_1,zed_2 = voxel_limits_z[j]
+        ix2 = (gal_zed[ix] >= zed_1) & (gal_zed[ix] < zed_2)
+        voxels[j] = (gal_zed[ix][ix2]).size
+        if voxels[j] > 0 :
+            voxelmeans[j] = (gal_zed[ix][ix2]).mean()
+    output.put((i, voxels,voxelmeans))
+
+
+
+
+if __name__ == '__main__':
+
+    hostloc = '/Volumes/ExtraSpace/DES_BLIND+HOSTZ'
+    hostlist = '/Volumes/ExtraSpace/DES_BLIND+HOSTZ/DES_BLIND+HOSTZ.LIST'
+
+    nohostloc = '/Volumes/ExtraSpace/DES_BLINDnoHOSTZ'
+    nohostlist = '/Volumes/ExtraSpace/DES_BLINDnoHOSTZ/DES_BLINDnoHOSTZ.LIST'
+
+    grabdata(hostloc,hostlist)
