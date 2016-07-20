017115014aa365a3332d9bbb172de24043292806
Dillon Brout <djbrout@gmail.com>
2016-07-20 10:09:53 -0400

    adding back in min for chisq params

--- makefueldata.py
+++ makefueldata.py
@@ -78,6 +78,9 @@ for param in range(big_data_array.shape[1]):
     ww = np.abs(big_data_array[:,param]) > 3*np.std(big_data_array[:,param])
     big_data_array[ww , param] = np.mean(big_data_array[~ww,param])
 
+    if param > 2:
+        big_data_array[:, param] = big_data_array[:,param] + np.min(big_data_array[:,param])
+
     plt.clf()
     plt.hist(big_data_array[:,param],bins=50)
     plt.savefig('paramplots/param_'+str(param)+'.png')
